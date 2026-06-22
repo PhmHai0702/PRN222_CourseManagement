48a1558de77d2167d68a5ef4961a75e8f768b621
khongthichcode <hmquang9805@gmail.com>
2026-06-22 09:59:09 +0700

    Merge pull request #9 from PhmHai0702/feat_coursecompare
    course comparision implementation

--- BlazorAppSecure/Program.cs
+++ BlazorAppSecure/Program.cs
@@ -3,6 +3,7 @@ using BlazorAppSecure.Services.Blog;
 using BlazorAppSecure.Sevices;
 using BlazorAppSecure.Sevices.Blog;
 using BlazorAppSecure.Sevices.Category;
+using BlazorAppSecure.Sevices.CourseComparison;
 using BlazorAppSecure.Sevices.Profile;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
@@ -28,6 +29,7 @@ builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStat
 builder.Services.AddScoped<IBlogService, BlogService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<CourseComparisonClient>();
 builder.Services.AddScoped(sp => (IAccountManagement)sp.GetRequiredService<AuthenticationStateProvider>());
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = 
